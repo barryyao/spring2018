34afa17db1443f1591669a588c7e0c85b6e3b32c
Yao Pan <yaopan2015@gmail.com>
2021-10-30 11:41:31 +0800

    delete checkifnone..

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,13 +1,6 @@
 public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char x, char y) {
-        if (checkIfNoneAlphabetical(x) || checkIfNoneAlphabetical(y)) {
-            throw new Error();
-        }
         return x - y == 1 || y - x == 1;
-    }
-
-    private boolean checkIfNoneAlphabetical(char a) {
-        return a > 'z' || a > 'Z' && a < 'a' || a < 'A';
     }
 }
--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -18,6 +18,6 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('A', 'A'));
         assertTrue(offByOne.equalChars('A', 'B'));
         assertFalse(offByOne.equalChars('A', 'b'));
-        assertFalse(offByOne.equalChars('&', '%'));
+        assertTrue(offByOne.equalChars('&', '%'));
     }
 }
