968a227c4a7d6f2a0e67cc36e46a98430292e5f5
Yao Pan <yaopan2015@gmail.com>
2021-10-31 18:06:07 +0800

    lab 3

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,6 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /** An Integer tester created by Flik Enterprises. */
 public class Flik {
     public static boolean isSameNumber(Integer a, Integer b) {
         return a == b;
+    }
+
+    @Test
+    public void testIsSameNumber() {
+        assertTrue(isSameNumber(1, 1));
+        assertFalse(isSameNumber(1, 2));
+        assertTrue(isSameNumber(127, 127));
+        assertFalse(isSameNumber(128, 128));
+        assertFalse(isSameNumber(129, 129));
     }
 }
--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -70,4 +70,15 @@ public class IntListTest {
       * to add a main method. See ArithmeticTest.java for an
       * example. */
 
+    @Test
+    public void testReverse() {
+        IntList A = IntList.of(1, 2, 3, 4, 5, 6);
+        IntList B = IntList.of(6, 5, 4, 3, 2, 1);
+        IntList C = IntList.reverse(A);
+        assertEquals(B, C);
+        assertNotEquals(A, C);
+        assertEquals(null, IntList.reverse(null));
+
+    }
+
 }
