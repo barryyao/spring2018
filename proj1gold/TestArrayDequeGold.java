b7aeceffc307aba2694575f239224d8f53f83bba
Yao Pan <yaopan2015@gmail.com>
2021-11-02 18:47:33 +0800

    proj1 gold

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,81 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class TestArrayDequeGold {
+
+    @Test
+    public void testStudentArrayDequeRadomly() {
+        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+        LinkedList<Integer> list = new LinkedList<>();
+        String errorMsg = "";
+        for (int i = 0; i < 20; i++) {
+            double uniform1 = StdRandom.uniform();
+            if (uniform1 > 0.75) {
+                sad1.addFirst(i);
+                list.add(i);
+                errorMsg += "addFirst(" + i + ")\n";
+            } else if (uniform1 > 0.5) {
+                sad1.addLast(i);
+                list.add(i);
+                errorMsg += "addLast(" + i + ")\n";
+            } else if (uniform1 > 0.25) {
+                Integer first = sad1.removeFirst();
+                errorMsg += "removeFirst(): " + first + "\n";
+                Integer remove = null;
+                if (list.size() > 0) {
+                    remove = list.remove(0);
+                }
+                assertEquals(errorMsg, first, remove);
+            } else {
+                Integer last = sad1.removeLast();
+                errorMsg += "removeLast(): " + last + "\n";
+                Integer remove = null;
+                if (list.size() > 0) {
+                    remove = list.removeLast();
+                }
+                assertEquals(errorMsg, last, remove);
+            }
+        }
+    }
+
+    @Test
+    public void testArrayDequeSolutionRadomly() {
+        ArrayDequeSolution<Integer> sad1 = new ArrayDequeSolution<>();
+        LinkedList<Integer> list = new LinkedList<>();
+        String errorMsg = "";
+        for (int i = 0; i < 20; i++) {
+            double uniform1 = StdRandom.uniform();
+            if (uniform1 > 0.75) {
+                sad1.addFirst(i);
+                list.add(i);
+                errorMsg += "addFirst(" + i + ")\n";
+            } else if (uniform1 > 0.5) {
+                sad1.addLast(i);
+                list.add(i);
+                errorMsg += "addLast(" + i + ")\n";
+            } else if (uniform1 > 0.25) {
+                Integer first = sad1.removeFirst();
+                errorMsg += "removeFirst(): " + first + "\n";
+                Integer remove = null;
+                if (list.size() > 0) {
+                    remove = list.remove(0);
+                }
+                assertEquals(errorMsg, first, remove);
+            } else {
+                Integer last = sad1.removeLast();
+                errorMsg += "removeLast(): " + last + "\n";
+                Integer remove = null;
+                if (list.size() > 0) {
+                    remove = list.removeLast();
+                }
+                assertEquals(errorMsg, last, remove);
+            }
+        }
+    }
+
+}
